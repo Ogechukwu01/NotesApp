9f95ccbd43ffdfadeb2c547e238add24e2d4d385
AntoniNeutron <antonineutron@gmail.com>
2023-12-07 12:08:43 +0100

    December 7th class - login page demo

--- app/src/main/java/com/wtm/wtmnotesapp/MainActivity.kt
+++ app/src/main/java/com/wtm/wtmnotesapp/MainActivity.kt
@@ -3,14 +3,23 @@ package com.wtm.wtmnotesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,6 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,17 +74,64 @@ fun LoginScreen(){
             value = username,
             onValueChange = {usernameInput -> username = usernameInput},
             label = {Text("Username")},
-            modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp)
         )
         TextField(
             value = password,
             onValueChange = {pwdInput -> password = pwdInput},
             label = {Text("Password")},
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp)
         )
+        RememberMe()
         Button(onClick = {}){
             Text(text="Login")
         }
+        Row{
+            Image(
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "Clickable facbook icon",
+                modifier = Modifier.size(24.dp)
+            )
+            Image(
+                painter =  painterResource(id = R.drawable.facebook),
+                contentDescription = "Clickable Google Icon",
+                modifier = Modifier.size(24.dp).clickable {
+                    //WHat happens when the image is tapped
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun RememberMe(){
+    var isChecked by remember{ mutableStateOf(false)}
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Text(text="Remember Me")
+        Switch(
+            checked = isChecked,
+            onCheckedChange = { isChecked = !isChecked}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        LoginScreen()
     }
 }
